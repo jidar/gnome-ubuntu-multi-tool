2d39623d82ed9239eaa8f52717d37f3afbbb1c52
Jose Idar <jose.idar@rackspace.com>
2014-04-27 17:35:15 -0500

    Added MANIFEST.in and removed pip-requires

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gumtool',
-    version='0.0.2',
+    version='0.0.1',
     description=(
         "A simple collection of tools that make running Gnome (3.10+) "
         "Ubuntu (14.04+) a little easier, especially on high-dpi hardware "
@@ -11,7 +11,7 @@ setup(
     author_email='jose.idar@gmail.com',
     url='blog.thatjoseguy.com',
     packages=find_packages(),
-    install_requires=open('pip-requires').read(),
+    install_requires=['argh'],
     license=open('LICENSE').read(),
     zip_safe=False,
     classifiers=(
